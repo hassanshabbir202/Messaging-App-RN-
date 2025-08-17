b2e4ab736a15b82d77a4184f1be4c0204e86887b
hassanshabbir202 <113466995+hassanshabbir202@users.noreply.github.com>
2025-08-17 14:34:50 +0500

    complete project setup

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+       <Text style={{textAlign:"center" , marginTop:40 , fontSize:25 , fontWeight:'bold'}}>Welcome Messaging App</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>CHAT SCREEN</Text>
+    </View>
+  )
+}
+
+export default ChatScreen
